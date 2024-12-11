c8a26c1320fe34bdc255b5b28c9af0ba635cc76e
rajon-git <rajon.zhsust15@gmail.com>
2024-12-11 15:56:52 +0600

    refresh token expired created

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/auth";
+import Cookies from 'js-cookie';
+import jwt-decode from 'jwt-decode';
 
 export const login = async (email, password) =>{
     try {
@@ -44,4 +46,59 @@ export const logout = () => {
     Cookies.remove("access_token")
     Cookies.remove("refresh_token")
     useAuthStore.getState().setUser(null)
+}
+
+export const setUser = async() => {
+    const accessToken  = Cookies.get("access_token")
+    const refreshToken  = Cookies.get("refresh_token")
+
+    if (!refreshToken || !accessToken) {
+        return;
+    }
+
+    if (isAccessTokenExpired(accessToken)) {
+        const response = await getRefreshToken(refreshToken)
+        setAuthUser(response.access, response.refresh)
+    }
+    else {
+        setAuthUser(accessToken, refreshToken)
+    }
+}
+
+export const setAuthUser = (access_token, refresh_token) => {
+    Cookies.set("access_token", access_token, {
+        expires: 1,
+        secure: true
+    })
+
+    Cookies.set("refresh_token", refresh_token, {
+        expires: 7,
+        secure: true
+    })
+
+    const user = jwt-decode(access_token) ?? null;
+
+    if (user) {
+        useAuthStore.getState().setUser(user)
+    }
+
+    useAuthStore.getState().setLoading(false)
+}
+
+export const getRefreshToken = async() => {
+    const refresh_token = Cookies.get("refresh_token")
+    const response = await axios.post('user/token/refresh/',{
+        refresh: refresh_token
+    })
+    return response.data;
+}
+
+export const isAccessTokenExpired = (accessToken) => {
+    try {
+        const decodedToken = jwt_decode(accessToken)
+        return decodedToken.exp < Date.now() / 100
+    } catch (error) {
+        console.log(error)
+        return true
+    }
 }
