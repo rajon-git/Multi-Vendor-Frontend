a3e1cd7eb84385d314d8ba2426805b50a20f91a3
rajon-git <rajon.zhsust15@gmail.com>
2024-12-11 15:25:53 +0600

    register functionality added

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useAuthStore } from "../store/auth";
 
-export const login = (email, password) =>{
+export const login = async (email, password) =>{
     try {
-        const {data, status} = axios.post("user/token/", {
+        const {data, status} = await axios.post("user/token/", {
             email,
             password
         })
@@ -16,6 +16,26 @@ export const login = (email, password) =>{
         return {
             data: null,
             error: error.response?.data?.detail || "Error occurred while trying to login"
+        }
+    }
+}
+
+export const register = async (full_name, email, phone, password, password2) => {
+    try {
+        const {data} =  await axios.post("user/register", {
+            full_name,
+            email,
+            phone,
+            password,
+            password2
+        })
+
+        await login(email, password)
+        return {data, error: null}
+    } catch (error) {
+        return {
+            data: null,
+            error: error.response?.data?.detail || "Error occurred while trying to register"
         }
     }
 }
